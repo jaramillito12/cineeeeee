fb973116286bcb77f5029ed2c7068daa5cb55d62
unknown <jaramillo>
2022-03-22 17:26:13 -0500

    reservas.js

---
+++ reservas.js
@@ -0,0 +1,17 @@
+let contenedor=document.getElementById("contenedor")
+    contenedor.addEventListener("click",function(evento){
+
+        if(evento.target.classList.contains("w-100")){
+
+            evento.target.src="imagenes.jpg/armchair.png"
+
+        }
+
+})
+
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+
+alert("Total:")
+
+})
