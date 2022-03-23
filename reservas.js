d4e4e05ced24d87668ae76b48e80c27241a747b0
jaramillo <jaramillito098x@gmail.com>
2022-03-23 17:13:13 -0500

    reservas.js

--- reservas.js
+++ reservas.js
@@ -1,17 +1,33 @@
-let contenedor=document.getElementById("contenedor")
-    contenedor.addEventListener("click",function(evento){
+let contenedor = document.getElementById("contenedor");
 
-        if(evento.target.classList.contains("w-100")){
+contenedor.addEventListener("click", function(evento){
+    if (evento.target.classList.contains("w-100")) {
+        evento.target.src="imagenes.jpg/armchair.png"
+        bandera = false
+    } else if(evento.target.classList.contains("w-100")) {
+        evento.target.src="imagenes.jpg/armchair.png"
+        bandera = false
+    }
+}) 
 
-            evento.target.src="imagenes.jpg/armchair.png"
+let envBtn = document.getElementById("boton");
+envBtn.addEventListener("click", escuchar);
 
-        }
+function escuchar(event) {
+    event.preventDefault();
+    let cantidad = document.getElementById("cantidad").value;
+    let asiento = document.getElementById("Asiento").value;
+    let boleta = 0
+    if(asiento == "general") {
+        boleta = 10000
+    } else {
+        boleta= 15000
+    }
 
-})
+    let total = boleta * cantidad;
 
-let boton=document.getElementById("boton")
-boton.addEventListener("click",function(evento){
-
-alert("Total:")
-
-})
+    
+    console.log(cantidad);
+    console.log(asiento);
+    alert("Precio total" + total + " pesos")
+}
